e57c9c49219e5b3535988d9a51e47777e9a72eaa
Maria Timis <maria90timis@yahoo.com>
2019-10-12 14:20:34 +0200

    changed to accept JSON input

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -7,13 +7,14 @@ app = Flask(__name__)
 def index():
     return "Server is running!"
 
-@app.route('/get/product/<int:id>', methods=['GET'])
-def get_product(id):
-    id = request.args.get('id', id)
+@app.route('/get/product/', methods=['GET'])
+def get_product():
+    id = request.json['id']
     return "Information about product with id {}!".format(id)
 
-@app.route('/put/order/<int:id>', methods=['GET', 'PUT', 'POST'])
-def put_order(id):
+@app.route('/put/order/', methods=['GET', 'PUT', 'POST'])
+def put_order():
+    id = request.json['id']
     return "Putting order with id {}".format(id)
 
 app.run(debug=True)
