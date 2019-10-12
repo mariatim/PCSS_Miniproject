9405775a260b87062bdc3941db106fda09a5b740
Oscar <objarn18@student.aau.dk>
2019-10-12 17:52:52 +0200

    Cancel is done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
-alert("something");
+function cancel (element) {
+    element.parentNode.parentNode.removeChild(element.parentNode);
+    /*var list = document.getElementById("mList");
+    while (list.hasChildNodes()) {
+        list.removeChild(list.firstChild);
+    }*/
+}
+
+
