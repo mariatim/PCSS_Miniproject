35d223715b04b88dc10b3929703e8fcbaaece981
Matyas <m.csepregi0@gmail.com>
2019-10-12 19:39:28 +0200

    Auto stash before merge of "dev" and "origin/dev"

---
+++ backend/Json.py
@@ -0,0 +1,38 @@
+import json
+
+
+class Json:
+
+    def __init__(self, _id, _name, _price, _description):
+        self.id = _id
+        self.name = _name
+        self.price = _price
+        self.description = _description
+
+    def setId(self, _id):
+        self.id = _id
+
+    def getId(self):
+        return self.id
+
+    def setName(self, _name):
+        self.name = _name
+
+    def getName(self):
+        return self.name
+
+    def setPrice(self, _price):
+        self.price = _price
+
+    def getPrice(self):
+        return self.price
+
+    def setDescription(self, _description):
+        self.id = _description
+
+    def getDescription(self):
+        return self.description
+
+    def toJson(self):
+        print(json.dumps(self.__dict__))
+        return json.dumps(self.__dict__)
--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request
+from backend.Json import Json
+import random
 
 app = Flask(__name__)
+products = []
 
 @app.route('/', methods=['GET'])
 def index():
@@ -9,7 +12,19 @@ def index():
 @app.route('/get/product/', methods=['GET'])
 def get_product():
     id = request.json['id']
-    return "Information about product with id {}!".format(id)
+    randomPos = random.randint(0, 5)
+    products[randomPos].setId(id)
+    return products[id].toJson()
+
+@app.route('/get/allproducts/', methods=['GET'])
+def get_all_products():
+    products[0] = Json(0, 'HyperFest', 1190, 'If you like rock music, this festival is for you!')
+    products[1] = Json(1, 'SuperFest', 1695, 'If you like boardgames, this festival is for you!')
+    products[2] = Json(2, 'MegaFest', 2100, 'If you like woodcarving, this festival is for you!')
+    products[3] = Json(3, 'GigaFest', 1099, 'If you like rock climbing, this festival is for you!')
+    products[4] = Json(4, 'GrandFest', 3000, 'If you like electronic music, this festival is for you!')
+    products[4] = Json(4, 'MegaloFest', 990, 'If you like aliens, this festival is for you!')
+    return products
 
 @app.route('/put/order/', methods=['GET', 'PUT', 'POST'])
 def put_order():
