bfa362ca3b9eb2f248150dc427a6aadc48cbd3be
Oscar <objarn18@student.aau.dk>
2019-10-12 19:58:34 +0200

    Updated cart math

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,18 @@
 function cancel (element) {
     element.parentNode.parentNode.removeChild(element.parentNode);
-    /*var list = document.getElementById("mList");
-    while (list.hasChildNodes()) {
-        list.removeChild(list.firstChild);
-    }*/
+
+    if (document.getElementById("cart").childElementCount == 0) {
+        document.getElementById("cartGuide").classList.toggle("invisible");
+        document.getElementById("price").innerHTML = "You have nothing in your cart!";
+    } else {
+        var canceledProduct = element.parentNode.childNodes[1].innerHTML;
+        console.log(canceledProduct);
+        var initPriceElementValue = document.getElementById("price").innerHTML;
+        var processedValue = parseFloat(initPriceElementValue.replace(" kr.", ""));
+        var processedPrice = parseFloat(canceledProduct.replace(" kr.", ""));
+        var output = processedValue - parseFloat(processedPrice) + " kr.";
+        document.getElementById("price").innerHTML = output;
+    }
 }
 
 
